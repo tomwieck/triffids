a910bed0490821cb01c4a4de6097e6a6e86c049c
joshJarr <josh.jarvis@potatolondon.com>
2018-11-24 16:08:54 +0000

    Added global header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <h1> Triffids. </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  width: 100%;
+  background: green;
+  left: 0;
+  top: 0;
+  position: fixed;
+  height: 60px;
+}
+</style>
