5f9c6ec23edad05cf828190033e43eb7632cf1a7
Tom Wieck <tom.wieck@potatolondon.com>
2018-11-25 11:43:27 +0000

    Add drawer styling with chevron

---
+++ src/components/Chevron.vue
@@ -0,0 +1,24 @@
+<template>
+<svg width="7px" height="10px" viewBox="0 0 7 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 52.4 (67378) - http://www.bohemiancoding.com/sketch -->
+    <title>ios-arrow-back</title>
+    <desc>Created with Sketch.</desc>
+    <g id="Views" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Park-view-with-info" transform="translate(-226.000000, -746.000000)" fill="#058E3F">
+            <g id="Group" transform="translate(0.000000, 337.000000)">
+                <g id="chevron-back-copy-2" transform="translate(229.000000, 414.000000) scale(-1, 1) translate(-229.000000, -414.000000) translate(219.000000, 404.000000)">
+                    <g id="ios-arrow-back" transform="translate(6.875000, 5.000000)">
+                        <path d="M1.71428571,4.97409326 L5.47742413,1.21391562 C5.75573649,0.935603257 5.75573649,0.485566247 5.47742413,0.210214656 C5.19911177,-0.0680977054 4.74907476,-0.0651369356 4.4707624,0.210214656 L0.207253886,4.4707624 C-0.0621761658,4.74019245 -0.0680977054,5.17246484 0.186528497,5.4507772 L4.46780163,9.74093264 C4.60695781,9.88008882 4.79052554,9.94818653 4.97113249,9.94818653 C5.15173945,9.94818653 5.33530718,9.88008882 5.47446336,9.74093264 C5.75277572,9.46262028 5.75277572,9.01258327 5.47446336,8.73723168 L1.71428571,4.97409326 Z" id="Path"></path>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+</template>
+
+<style scoped>
+  .rotate {
+    transform: rotate(90deg);
+  }
+</style>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,11 +1,43 @@
 <template>
   <div id="drawer" class="drawer">
-    <button @click="greet"> Ello </button>
-    <h1> Drawer. </h1>
+    <button class="drawer__button" @click="greet">
+      <Chevron class="rotate"/>
+    </button>
+    <div class="drawer__trees">
+      <div>
+        <div>
+          <span>13</span>
+          Unique species
+        </div>
+        <div>
+          <span>143</span>
+          Trees in total
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div class="drawer__about">
+      <h1> About Victoria Park </h1>
+      <ul>
+        <li>Free admission</li>
+        <li>Open at all times</li>
+      </ul>
+      <p>
+        The park was established in the 1880s following the expansion of Bedminster as a residential and industrial area within Bristol. The council bought 51.5 acres (20.8 ha) of land from Sir John Henry Greville Smyth for £20,678 (now £2,110,000), though the land had been used as an unofficial open space and meeting area for some time before this. By 1887, a children's play area had been installed which became immediately popular. The streets around the park were laid out in 1891. By 1898, four rangers were permanently employed in the park, and a bandstand had been installed. Several drinking fountains and a circular pond had also been established.
+      </p>
+      <hr />
+      <div class="drawer__report">
+        <a target="_blank" href="https://www.bristol.gov.uk/museums-parks-sports-culture/parks-and-open-spaces">
+          Report an issue with this facility <Chevron />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Chevron from './Chevron.vue'
+
 export default {
   name: 'drawer',
   data: () => {
@@ -13,9 +45,11 @@ export default {
     el: '#drawer',
     }
   },
+  components: {
+    Chevron
+  },
   methods: {
-    greet: function (event) {
-
+    greet: function () {
       const drawer = document.getElementById('drawer')
       drawer.classList.contains('expanded') ? drawer.classList.remove('expanded') : drawer.classList.add('expanded')
     }
@@ -26,17 +60,75 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .drawer {
-  width: 100%;
-  background: #eee;
-  border-radius: 20px 20px 0 0;
+  background: #fff;
+  box-shadow: 0 2px 4px;
   left: 0;
   bottom: 0;
   position: fixed;
   transition: all .5s ease;
-  height: 60px;
+  height: 50%;
+  overflow-y: scroll;
+  padding: 16px;
 
   &.expanded {
-    height: 300px;
+    height: 40px;
+  }
+
+  &__button {
+    border: none;
+    cursor: pointer;
+    width: 100%;
+  }
+
+  &__about {
+    text-align: left;
+  }
+
+  &__report {
+    a {
+      color: #058E3F;
+      font-family: 'Helvetica';
+      text-decoration: none;
+    }
+  }
+
+}
+
+h1 {
+  color: #058E3F;
+  font-family: 'Helvetica';
+  font-weight: 400;
+}
+
+hr {
+  border: 1px solid;
+  color: #EBEBEC;
+}
+
+ul {
+  list-style-type: none;
+  display: inline;
+  padding-inline-start: 0;
+}
+
+li {
+  display: inline;
+  position: relative;
+
+  &:not(:first-child) {
+    padding-left: 18px;
+
+    &:after {
+      background-color: #4F6272;
+      content: '';
+      display: inline-block;
+      border-radius: 20px;
+      height: 6px;
+      width: 6px;
+      position: absolute;
+      left: 6px;
+      top: 8px;
+    }
   }
 }
 </style>
