0fd1289f9bc4409c5c968a04c338144f108f89e2
jalal (Tim Pushman) <thpushman@gmail.com>
2018-11-25 11:12:45 +0000

    fixup

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,23 @@
 <template>
   <div id="app">
     <Header />
-<<<<<<< HEAD
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <hr>
-    <Tmap />
-
-=======
     <router-view></router-view>
     <Drawer />
->>>>>>> master
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-<<<<<<< HEAD
 import Tmap from './components/Tmap.vue'
-=======
 import Drawer from './components/Drawer.vue'
->>>>>>> master
 
 export default {
   name: 'app',
   components: {
-<<<<<<< HEAD
     HelloWorld,
     Header,
-    Tmap
-=======
-    Header,
+    Tmap,
     Drawer
->>>>>>> master
   }
 }
 </script>
