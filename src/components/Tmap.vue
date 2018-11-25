cf68a545c7dd895271b403d3a2a4fd4ae64f1bc9
jalal (Tim Pushman) <thpushman@gmail.com>
2018-11-25 14:21:45 +0000

    Use Bristol OD to pull trees.

--- src/components/Tmap.vue
+++ src/components/Tmap.vue
@@ -8,14 +8,31 @@
 <script>
 const lat = 51.44080;
 const lng = -2.58889;
+var mymap;
 const testUrl = `https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=trees&q=site_code%3DVICTPA&facet=feature_type_name&facet=common_name`;
 let trees = axios.get(testUrl)
   .then((resp) => {
-      const l = resp.data.records.map((item, idx) => {
-        return item.fields.geo_point_2d;
+      const list = resp.data.records.map((item, idx) => {
+        return item.fields;
       })
-    console.log(l);
+      console.log(list);
+      loadTrees(list);
   });
+
+function loadTrees(trees) {
+    mymap = L.map('mapid').setView([lat, lng], 16);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiamFsYWxza2kiLCJhIjoiY2pvd3d3bm40MXcxczNrbzlyazB2ZDVobyJ9.wV99nDE6dCfGjMUrEIjgFQ'
+    }).addTo(mymap);
+    trees.forEach(t => {
+      let marker = L.marker([t['geo_point_2d'][0], t['geo_point_2d'][1]]).addTo(mymap);
+      marker.bindPopup(t['common_name']);
+    });
+}
+
 // let tree1 = trees[0];
 // console.log('Tree 1: ' + trees);
 export default {
@@ -25,21 +42,6 @@ export default {
     }
   },
   mounted: function () {
-    const mymap = L.map('mapid').setView([lat, lng], 16);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoiamFsYWxza2kiLCJhIjoiY2pvd3d3bm40MXcxczNrbzlyazB2ZDVobyJ9.wV99nDE6dCfGjMUrEIjgFQ'
-    }).addTo(mymap);
-    var marker = L.marker([51.44246819415003, -2.58468603740490]).addTo(mymap);
-    marker.bindPopup("Hi, I'm a Scots Pine tree!").openPopup();
-    marker = L.marker([51.43851539948433, -2.5838943269219232]).addTo(mymap);
-    marker.bindPopup("Hi, I'm an Aspen tree!");
-    marker = L.marker([51.439150727525785, -2.5870542824523888]).addTo(mymap);
-    marker.bindPopup("Hi, I'm a Hollybush tree!");
-    marker = L.marker([51.439650474460116, -2.588689907775596]).addTo(mymap);
-    marker.bindPopup("Hi, I'm an Oak tree!");
   },
   name: 'Tmap',
 }
