72478f0e2162e5d28df1fe15d9ba7472ce7af3f7
jalal (Tim Pushman) <thpushman@gmail.com>
2018-11-25 11:01:40 +0000

    Added Map component

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,23 @@
     <Header />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr>
+    <Tmap />
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
+import Tmap from './components/Tmap.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    Header
+    Header,
+    Tmap
   }
 }
 </script>
