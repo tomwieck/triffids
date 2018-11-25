bcad62f7a25de474042fe8b2a8d351297ff78345
jalal (Tim Pushman) <thpushman@gmail.com>
2018-11-25 11:02:19 +0000

    Added Tmap component

---
+++ src/components/Tmap.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Map</h1>
+        <Tmap class="tmap"></Tmap>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Tmap',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+tmap {
+  width: 100%;
+  left: 0;
+  top: 0;
+}
+</style>
