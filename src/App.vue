39b3360dcd61046a09bab3eeb5bec03427694ab2
joshJarr <josh.jarvis@potatolondon.com>
2019-01-25 14:59:01 +0000

    Split styles into importable sheets.

--- src/App.vue
+++ src/App.vue
@@ -13,22 +13,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-body {
-  margin: 0;
-}
-
-.layer {
-  box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.25);
-}
-
+<style lang="scss">
+  @import  'styles/variables.scss';
+  @import  'styles/normalize.scss';
+  @import  'styles/utils.scss';
 </style>
