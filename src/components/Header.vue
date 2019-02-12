d025c3d853bf098a2bb778058cba84f0feb3c4c4
jalal (Tim Pushman) <thpushman@gmail.com>
2019-02-12 11:33:32 +0000

    Revert back arrow to correct style.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -39,17 +39,15 @@ export default {
   font-size: 0.8em;
 }
 .back-btn {
-  background-image: url("../assets/backwoods.svg");
   width: 32px;
   height: 32px;
   position: absolute;
-  bottom: 10%;
+  top: 10%;
   left: 1%;
   font-size: 3em;
   color: white;
 }
-/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> 
-from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> 
-is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">
-CC 3.0 BY</a></div> */
+.back-btn:after {
+  content: "\02c2";
+}
 </style>
