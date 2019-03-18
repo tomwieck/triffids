c7fa7bfa9bafe0beabd9eec2bd55881994404b39
jalal (Tim Pushman) <thpushman@gmail.com>
2019-03-18 19:42:35 +0000

    FIXES #72 also some query optimisations

--- server/triffidsapi/trees.py
+++ server/triffidsapi/trees.py
@@ -1,19 +1,19 @@
 import json
 import requests
-import os
+# import os
 
 # Read trees.json
-baseDirectory = os.path.join(os.path.dirname(__file__), '..')
+# baseDirectory = os.path.join(os.path.dirname(__file__), '..')
 
-with open(baseDirectory + '/data/trees.json') as json_file:
-    data = json.load(json_file)
+# with open(baseDirectory + '/data/trees.json') as json_file:
+#     data = json.load(json_file)
 
 url = "https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=trees"
 
 
 def getTreeById(id):
     query = "&q=recordid%3D" + str(id) + \
-            "&facet=feature_type_name&facet=common_name&refine.feature_type_name=Tree+-+Parks+and+Green+Space"
+        "&refine.feature_type_name=Tree+-+Parks+and+Green+Space"
 
     response = requests.get(url + query)
     response = response.json()
@@ -26,7 +26,10 @@ def getTreeById(id):
 
 
 def getTreesByPark(parkCode):
-    query = "&q=site_code%3D" + str(parkCode) + "&rows=1000" + "&facet=feature_type_name&facet=common_name"
+    fields = "site_code,geo_point_2d,latin_code,common_name,full_common_name,dbh,crown_area"
+    query = "&q=site_code%3D" + \
+        str(parkCode) + "&rows=1000" + \
+        "&fields=" + fields
 
     response = requests.get(url + query)
     response = response.json()
@@ -38,24 +41,20 @@ def getTreesByPark(parkCode):
         return []
 
 
-
-
 def getTreeNumbers(parkCode):
     trees = getTreesByPark(parkCode)
 
-    if trees:
-        totalTrees = len(trees)
-    else:
-        totalTrees = 0
-
+    totalTrees = len(trees)
     uniqueSpecies = len(getUniqueSpecies(trees))
 
     return totalTrees, uniqueSpecies
 
 
 def getTreesBySpecies(parkCode, latinCode):
-    query = "&q=site_code%3D" + str(parkCode) + "%2C+latin_code%3D" + str(latinCode) + \
-            "&rows=1000&facet=feature_type_name&facet=common_name"
+    fields = "site_code,latin_code,common_name,full_common_name,dbh,crown_area"
+    query = "&q=site_code%3D" + str(parkCode) + "%2C+latin_code%3D" + \
+            str(latinCode) + "&fields=" + fields + \
+            "&rows=1000"
 
     response = requests.get(url + query)
     response = response.json()
@@ -67,11 +66,11 @@ def getTreesBySpecies(parkCode, latinCode):
         return []
 
 
-
 def getTreesByLocation(lat, lng, radius):
     trees = []
 
-    query = "&geofilter.distance=" + str(lat) + "%2C+" + str(lng) + "%2C+" + str(radius) + "&rows=1000"
+    query = "&geofilter.distance=" + \
+        str(lat) + "%2C+" + str(lng) + "%2C+" + str(radius) + "&rows=1000"
 
     response = requests.get(url + query)
     response = response.json()
@@ -91,17 +90,14 @@ def getUniqueSpecies(trees):
     # Get all trees within park
     # trees = getTreesByPark(parkCode)
 
-    species = []
+    species = set()
 
     # Extract species of every tree in park
     if trees:
         for tree in trees:
-            species.append(tree['fields']['latin_code'])
-    else:
-        return []
+            if 'latin_code' in tree['fields']:
+                species.add(tree['fields']['latin_code'])
 
-    # Convert list to set to get all unique instances of species
-    species = set(species)
     return species
 
 # print(getTreesByPark("VICTPA"))
